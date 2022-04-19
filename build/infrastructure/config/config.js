44d9752895c09083ed3c4a3cdf43b8950422a8c7
guinacosta <agnaldo.almeida@iteris.com.br>
2022-04-19 10:52:34 -0300

    change secret back to string type

--- build/infrastructure/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONFIGURATIONS = void 0;
-exports.CONFIGURATIONS = {
-    SO_API_URL: process.env.SO_API_URL || "",
-    SO_API_TOKEN: process.env.SO_API_TOKEN || "",
-    SECRET_MANAGER_ARN: process.env.SECRET_MANAGER_ARN || "",
-};
